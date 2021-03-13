485bffef5dc054eefa8b61495f40f07bf1a1a8fe
Anwesh43 <anweshthecool0@gmail.com>
2021-03-13 16:07:49 +0530

    using cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 const fastify = require('fastify')({logger: true})
+const fastifyCors = require('fastify-cors')
 const start = async(port) => {
     console.log('server has started')
+    fastify.register(fastifyCors)
     fastify.register(require('./createEndpointsFromFiles'))
     await fastify.ready((err) => {
         console.log(err)
